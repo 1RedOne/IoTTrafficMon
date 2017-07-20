37dcb5e8132cab99af153b71494efab860083569
Stephen Owen <sowen@ivision.com>
2017-07-20 13:30:12 -0400

    added Work in progress example

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This page hosts the repository of code covered in this [blogpost](http://wp.me/p3Q7Nu-1gX) on FoxDeploy.com.
 
 You are free to use and modify this code in any way, and I hope you do!  If you create a cool monitoring anything, please share examples or screenshots of what you created in the .\LookWhatIMade folder!
+
+The .\WIP folder contains a Point-in-time example of the code, referenced in the post as well.
---
+++ WIP/Stage_I/IotGetStats/IoTCoreDefaultApp/StatInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses
+{
+    public class StatObject
+    {
+        public string date { get; set; }
+        public string period { get; set; }
+        public int views { get; set; }
+        public int visitors { get; set; }
+        public int likes { get; set; }
+        public int reblogs { get; set; }
+        public int comments { get; set; }
+        public int followers { get; set; }
+    }
+}
